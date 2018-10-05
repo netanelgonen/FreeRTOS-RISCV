d915911f17a6ce0a22c399975f3330a1deabd0c0
Michal Podhradsky <mpodhradsky@galois.com>
2018-10-05 13:12:02 -0700

    Add uart support

--- Demo/riscv-spike/arch/syscalls.c
+++ Demo/riscv-spike/arch/syscalls.c
@@ -1,171 +1,167 @@
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include "ns16550.h"
+
+#ifndef __linux__
+
+int read(int file, char *ptr, int len) {
+  int todo;
+  if(len == 0)
+    return 0;
+#ifdef CONSOLE_UART
+  todo = 0;
+  while (ns16550_rxready() && (todo < len)) {
+    *ptr++ = ns16550_rxchar();
+    todo++;
+  }
+#endif
+  return todo;
+}
+
+int write(int file, char *ptr, int len) {
+  int todo;
+
+#ifdef CONSOLE_UART
+  for (todo = 0; todo < len; todo++) {
+    ns16550_txchar (*ptr++);
+  }
+#endif
+  return len;
+}
+
+#define CLOCK_PERIOD  (30000000)
+//#define CLOCK_PERIOD  (100) // simulation
+
+// The following uses rdcycle instead of rdtime (which would make more sense),
+// because rdtime causes an "illegal instruction" trap, which we do not yet
+// handle in bare metal.
+
+int gettimeofday(struct timeval *ptimeval, void *ptimezone)
+{
+    if (ptimeval)
+    {
+	long long tv;
+#if __riscv_xlen == 64
+	asm ("rdcycle %0" : "=r" (tv));
+#else
+	unsigned int tvh;
+	unsigned int tvl;
+	asm ("rdcycle %0;"
+	    "rdcycleh %1 " : "=r" (tvl), "=r" (tvh));
+	tv = ((long long)tvh) << 32 | tvl;
+#endif
+	ptimeval->tv_sec = tv / CLOCK_PERIOD;
+	ptimeval->tv_usec = tv % CLOCK_PERIOD / (CLOCK_PERIOD / 1000000);
+    }
+
+    return 0;
+}
+
+unsigned int sleep(unsigned int seconds)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    seconds += tv.tv_sec;
+
+    while (tv.tv_sec < seconds)
+	gettimeofday(&tv, NULL);
+
+    return 0;
+}
+
+// JES drafts:
+
+void *
+_sbrk (int nbytes)
+{
+  errno = ENOMEM;
+  return  (void *) -1;
+}
+
 /*
-    FreeRTOS V8.2.3 - Copyright (C) 2015 Real Time Engineers Ltd.
-    All rights reserved
-
-    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
-
-    This file is part of the FreeRTOS distribution and was contributed
-    to the project by Technolution B.V. (www.technolution.nl,
-    freertos-riscv@technolution.eu) under the terms of the FreeRTOS
-    contributors license.
-
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-
-    ***************************************************************************
-    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
-    >>!   distribute a combined work that includes FreeRTOS without being   !<<
-    >>!   obliged to provide the source code for proprietary components     !<<
-    >>!   outside of the FreeRTOS kernel.                                   !<<
-    ***************************************************************************
-
-    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
-    link: http://www.freertos.org/a00114.html
-
-    ***************************************************************************
-     *                                                                       *
-     *    FreeRTOS provides completely free yet professionally developed,    *
-     *    robust, strictly quality controlled, supported, and cross          *
-     *    platform software that is more than just the market leader, it     *
-     *    is the industry's de facto standard.                               *
-     *                                                                       *
-     *    Help yourself get started quickly while simultaneously helping     *
-     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
-     *    tutorial book, reference manual, or both:                          *
-     *    http://www.FreeRTOS.org/Documentation                              *
-     *                                                                       *
-    ***************************************************************************
-
-    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
-    the FAQ page "My application does not run, what could be wrong?".  Have you
-    defined configASSERT()?
-
-    http://www.FreeRTOS.org/support - In return for receiving this top quality
-    embedded software for free we request you assist our global community by
-    participating in the support forum.
-
-    http://www.FreeRTOS.org/training - Investing in training allows your team to
-    be as productive as possible as early as possible.  Now you can receive
-    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
-    Ltd, and the world's leading authority on the world's leading RTOS.
-
-    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
-    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
-    compatible FAT file system, and our tiny thread aware UDP/IP stack.
-
-    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
-    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
-
-    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
-    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
-    licenses offer ticketed support, indemnification and commercial middleware.
-
-    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
-    engineered and independently SIL3 certified version for use in safety and
-    mission critical applications that require provable dependability.
-
-    1 tab == 4 spaces!
+int _write(
+   int fd,
+   const void *buffer,
+   unsigned int count
+	   )
+{
+  errno = ENOSPC;
+  return -1;
+}
 */
 
-#include <stdint.h>
-#include <string.h>
-#include "syscalls.h"
-#include "encoding.h"
-#include "clib.h"
+int _write(int file, char *ptr, int len) {
+  int todo;
 
-volatile uint64_t tohost __attribute__((aligned(64)));
-volatile uint64_t fromhost __attribute__((aligned(64)));
-
-uint64_t zeroExtend(long val)
-{
-	uint64_t ret = val;
-	#if __riscv_xlen == 32
-		ret = (0x00000000ffffffff & val);
-	#endif
-	return ret;
+#ifdef CONSOLE_UART
+  for (todo = 0; todo < len; todo++) {
+    ns16550_txchar (*ptr++);
+  }
+#endif
+  return len;
 }
 
-/* Relay syscall to host */
-static uint64_t prvSyscallToHost(long which, long arg0, long arg1, long arg2)
+int _close(
+   int fd
+	   )
 {
-	volatile uint64_t magic_mem[8] __attribute__((aligned(64)));
-//    volatile uint64_t oldfromhost;
-	magic_mem[0] = zeroExtend(which);
-	magic_mem[1] = zeroExtend(arg0);
-	magic_mem[2] = zeroExtend(arg1);
-	magic_mem[3] = zeroExtend(arg2);
-	__sync_synchronize();
-    tohost = zeroExtend(magic_mem);
-//    do
-//    {
-//        oldfromhost = fromhost;
-//        fromhost = 0;
-//    } while (oldfromhost == 0);
-	return magic_mem[0];
+  errno = EBADF;
+  return -1;
 }
-/*-----------------------------------------------------------*/
 
-/* Exit systemcall */
-static void prvSyscallExit(long code)
+long _lseek(
+    int fd,
+    long offset,
+    int origin
+)
 {
-	uint64_t zcode = zeroExtend(code);
-	tohost = ((zcode << 1) | 1);
-	for(;;) { }
+  errno = EBADF;
+  return -1;
 }
-/*-----------------------------------------------------------*/
 
-/* Prints a string with a syscall  */
-static void printstr(const char* s)
+int _read(
+   int fd,
+   void *buffer,
+   unsigned int count
+)
 {
-	syscall(SYS_write, 1, (long) s, strlen(s));
+  errno = EBADF;
+  return -1;
 }
-/*-----------------------------------------------------------*/
 
-/* Fires a syscall */
-long syscall(long num, long arg0, long arg1, long arg2)
+int _fstat(
+   int fd,
+   struct _stat *buffer
+)
 {
-	register long a7 asm("a7") = num;
-	register long a0 asm("a0") = arg0;
-	register long a1 asm("a1") = arg1;
-	register long a2 asm("a2") = arg2;
-	asm volatile ("scall":"+r"(a0) : "r"(a1), "r"(a2), "r"(a7));
-	return a0;
+  errno = EBADF;
+  return -1;
 }
-/*-----------------------------------------------------------*/
 
-/* Programs need to override this function. */
-int __attribute__((weak)) main(__attribute__ ((unused)) int argc, __attribute__ ((unused)) char** argv)
+int _isatty(
+  int fd
+)
 {
-	printstr("Implement a main function!\n");
-	return -1;
+  errno = EBADF;
+  return 0;
 }
-/*-----------------------------------------------------------*/
 
-/* Starts main function. */
-void vSyscallInit(void)
+int _kill(
+  int pid,
+  int sig
+)
 {
-	int ret = main(0, 0);
-	exit(ret);
+  errno = EBADF;
+  return -1;
 }
-/*-----------------------------------------------------------*/
 
-/* Trap handeler */
-unsigned long ulSyscallTrap(long cause, long epc, long regs[32])
+int _getpid(
+  int n
+)
 {
-	long returnValue = 0;
-
-	if (cause != CAUSE_MACHINE_ECALL) {
-		prvSyscallExit(cause);
-	} else if (regs[17] == SYS_exit) {
-		prvSyscallExit(regs[10]);
-	} else {
-		returnValue = prvSyscallToHost(regs[17], regs[10], regs[11], regs[12]);
-	}
-
-	regs[10] = returnValue;
-	return epc + 4;
+  return 1;
 }
-/*-----------------------------------------------------------*/
+
+#endif
