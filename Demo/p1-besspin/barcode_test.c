0b2ae80c56db1f3b81421ab5e5cf73ffa6ed11c8
cgoins <cgoins@galois.com>
2019-02-12 10:52:34 -0800

    Modified UART driver to use 2 UART peripherals

---
+++ Demo/p1-besspin/barcode_test.c
@@ -0,0 +1,204 @@
+/*
+    FreeRTOS V8.2.3 - Copyright (C) 2015 Real Time Engineers Ltd.
+    All rights reserved
+
+    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
+    This file is part of the FreeRTOS distribution and was contributed
+    to the project by Technolution B.V. (www.technolution.nl,
+    freertos-riscv@technolution.eu) under the terms of the FreeRTOS
+    contributors license.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
+
+    ***************************************************************************
+    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
+    >>!   distribute a combined work that includes FreeRTOS without being   !<<
+    >>!   obliged to provide the source code for proprietary components     !<<
+    >>!   outside of the FreeRTOS kernel.                                   !<<
+    ***************************************************************************
+
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
+    link: http://www.freertos.org/a00114.html
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS provides completely free yet professionally developed,    *
+     *    robust, strictly quality controlled, supported, and cross          *
+     *    platform software that is more than just the market leader, it     *
+     *    is the industry's de facto standard.                               *
+     *                                                                       *
+     *    Help yourself get started quickly while simultaneously helping     *
+     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
+     *    tutorial book, reference manual, or both:                          *
+     *    http://www.FreeRTOS.org/Documentation                              *
+     *                                                                       *
+    ***************************************************************************
+
+    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
+    the FAQ page "My application does not run, what could be wrong?".  Have you
+    defined configASSERT()?
+
+    http://www.FreeRTOS.org/support - In return for receiving this top quality
+    embedded software for free we request you assist our global community by
+    participating in the support forum.
+
+    http://www.FreeRTOS.org/training - Investing in training allows your team to
+    be as productive as possible as early as possible.  Now you can receive
+    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
+    Ltd, and the world's leading authority on the world's leading RTOS.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+    compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
+    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
+    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
+
+    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
+    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
+    licenses offer ticketed support, indemnification and commercial middleware.
+
+    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+    engineered and independently SIL3 certified version for use in safety and
+    mission critical applications that require provable dependability.
+
+    1 tab == 4 spaces!
+*/
+
+/*
+ * main() creates a task to run UART tests.
+ * It then starts the scheduler.
+ */
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+
+/* Common demo includes. */
+#include "blocktim.h"
+#include "countsem.h"
+#include "recmutex.h"
+
+/* RISCV includes */
+#include <stdio.h>
+#include <stdint.h>
+#include "riscv_counters.h"
+#include "uart_16550.h"
+
+/* A block time of zero simply means "don't block". */
+#define mainDONT_BLOCK						( 0UL )
+
+/*-----------------------------------------------------------*/
+
+/*
+ * FreeRTOS hook for when malloc fails, enable in FreeRTOSConfig.
+ */
+void vApplicationMallocFailedHook( void );
+
+/*
+ * FreeRTOS hook for when freertos is idling, enable in FreeRTOSConfig.
+ */
+void vApplicationIdleHook( void );
+
+/*
+ * FreeRTOS hook for when a stackoverflow occurs, enable in FreeRTOSConfig.
+ */
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+
+/*
+ * Barcode test task to be recieve barcode from barcode scanner 
+ */
+void vTestBarcode( void *pvParameters );
+
+/*-----------------------------------------------------------*/
+/* main entry point */
+
+int main( void )
+{	
+	/* Create UART test */
+	xTaskCreate( vTestBarcode, "Barcode Test", 1000, NULL, 0, NULL );
+
+	/* Start the kernel.  From here on, only tasks and interrupts will run. */
+	vTaskStartScheduler();
+
+	/* Exit FreeRTOS */
+	return 0;
+}
+/*-----------------------------------------------------------*/
+void vTestBarcode( void *pvParameters )
+{
+	/* vTestBarcode() tests the CCD PS/2 barcode scanner
+	and sends the output it receives from it to the USB-UART
+	serial output.
+	UART0 = USB-UART
+	UART1 = PMOD UART (barcode scanner)  */
+
+	int i;
+	char uart_rx_str[11];
+
+	/* Initalize UARTs */
+	uart0_init();
+	uart1_init();
+
+	/* Receive barcode */	
+	/* Loop 10 times to receive 10 chars of barcode */
+	for(i=0; i<10; i++) {
+		uart_rx_str[i] = uart1_rxchar();
+	}
+	/* Add end of string character */
+	uart_rx_str[10] = '\0';
+
+	/* Print barcode via UART0 */
+	printf( uart_rx_str );
+}
+/*-----------------------------------------------------------*/
+void vApplicationMallocFailedHook( void )
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.
+	pvPortMalloc() is called internally by the kernel whenever a task, queue,
+	timer or semaphore is created.  It is also called by various parts of the
+	demo application.  If heap_1.c or heap_2.c are used, then the size of the
+	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+	to query the size of free heap space that remains (although it does not
+	provide information on how the remaining heap might be fragmented). */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	task.  It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()).  If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
---
+++ Demo/p1-besspin/spi_test.c
@@ -0,0 +1,242 @@
+/*
+    FreeRTOS V8.2.3 - Copyright (C) 2015 Real Time Engineers Ltd.
+    All rights reserved
+
+    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
+    This file is part of the FreeRTOS distribution and was contributed
+    to the project by Technolution B.V. (www.technolution.nl,
+    freertos-riscv@technolution.eu) under the terms of the FreeRTOS
+    contributors license.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
+
+    ***************************************************************************
+    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
+    >>!   distribute a combined work that includes FreeRTOS without being   !<<
+    >>!   obliged to provide the source code for proprietary components     !<<
+    >>!   outside of the FreeRTOS kernel.                                   !<<
+    ***************************************************************************
+
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
+    link: http://www.freertos.org/a00114.html
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS provides completely free yet professionally developed,    *
+     *    robust, strictly quality controlled, supported, and cross          *
+     *    platform software that is more than just the market leader, it     *
+     *    is the industry's de facto standard.                               *
+     *                                                                       *
+     *    Help yourself get started quickly while simultaneously helping     *
+     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
+     *    tutorial book, reference manual, or both:                          *
+     *    http://www.FreeRTOS.org/Documentation                              *
+     *                                                                       *
+    ***************************************************************************
+
+    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
+    the FAQ page "My application does not run, what could be wrong?".  Have you
+    defined configASSERT()?
+
+    http://www.FreeRTOS.org/support - In return for receiving this top quality
+    embedded software for free we request you assist our global community by
+    participating in the support forum.
+
+    http://www.FreeRTOS.org/training - Investing in training allows your team to
+    be as productive as possible as early as possible.  Now you can receive
+    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
+    Ltd, and the world's leading authority on the world's leading RTOS.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+    compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
+    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
+    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
+
+    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
+    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
+    licenses offer ticketed support, indemnification and commercial middleware.
+
+    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+    engineered and independently SIL3 certified version for use in safety and
+    mission critical applications that require provable dependability.
+
+    1 tab == 4 spaces!
+*/
+
+/*
+ * main() creates a task to run UART tests.
+ * It then starts the scheduler.
+ */
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+
+/* Common demo includes. */
+#include "blocktim.h"
+#include "countsem.h"
+#include "recmutex.h"
+
+/* RISCV includes */
+#include <stdio.h>
+#include <stdint.h>
+#include "riscv_counters.h"
+#include "uart_16550.h"
+
+/* A block time of zero simply means "don't block". */
+#define mainDONT_BLOCK						( 0UL )
+
+#define SPI_BASE (0x62320000ULL)
+#define SPICR (*(unsigned int *) 0x62320060)
+#define SPISR (*(unsigned int *) 0x62320064)
+#define SPI_DTR (*(unsigned int *) 0x62320068)
+#define SPI_DRR (*(unsigned int *) 0x6232006c)
+#define SPI_SSR (*(unsigned int *) 0x62320070)
+
+/*-----------------------------------------------------------*/
+
+/*
+ * FreeRTOS hook for when malloc fails, enable in FreeRTOSConfig.
+ */
+void vApplicationMallocFailedHook( void );
+
+/*
+ * FreeRTOS hook for when freertos is idling, enable in FreeRTOSConfig.
+ */
+void vApplicationIdleHook( void );
+
+/*
+ * FreeRTOS hook for when a stackoverflow occurs, enable in FreeRTOSConfig.
+ */
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+
+/*
+ * UART test task to be used with TestGfe.test_uart_driver
+ * in test_gfe_unittest.py 
+ */
+void vTestSPI( void *pvParameters );
+
+/*-----------------------------------------------------------*/
+/* main entry point */
+
+int main( void )
+{	
+  uart_init();
+
+	/* Create SPI test */
+	xTaskCreate( vTestSPI, "SPI Test", 1000, NULL, 0, NULL );
+
+	/* Start the kernel.  From here on, only tasks and interrupts will run. */
+	vTaskStartScheduler();
+
+	/* Exit FreeRTOS */
+	return 0;
+}
+/*-----------------------------------------------------------*/
+void vTestSPI( void *pvParameters )
+{
+	/* vTestSPI() tests the AXI SPI on the VCU118 in 
+  loopback mode */
+
+  char spi_rx;
+
+	/***** INIT *****/
+	// Set CPHA=0 and CPOL=0
+  SPICR &= ~(0x18);
+
+  // Reset TX FIFO
+  SPICR |= 0x20;
+
+  // Set to Master mode
+  SPICR |= 0x4;
+
+  // Enable loopback mode (LOOP)
+  SPICR |= 0x1;
+
+  // SPI system enable (SPE)
+  SPICR |= 0x2;
+
+	/***** TRANSMIT *****/
+	// Inhibit master
+  SPICR |= 0x100;
+
+  // Write data to data transmit register
+  SPI_DTR = 'a';
+
+  // Issue chip select
+  SPI_SSR = 0x0;
+
+  // Deinhibit master
+  SPICR &= 0x011;
+
+  // Deassert chip select
+  SPI_SSR = 0x1;
+
+  // Inhibit master
+  SPICR |= 0x100;
+
+  // While RX empty is true, wait
+  while ( (SPISR & 0x1) != 0 )
+     ; // wait
+
+  spi_rx = SPI_DRR;
+
+  // Read
+  printf( "Received: %c \n", spi_rx);
+
+  vTaskDelete( NULL );
+}
+
+/*-----------------------------------------------------------*/
+void vApplicationMallocFailedHook( void )
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.
+	pvPortMalloc() is called internally by the kernel whenever a task, queue,
+	timer or semaphore is created.  It is also called by various parts of the
+	demo application.  If heap_1.c or heap_2.c are used, then the size of the
+	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+	to query the size of free heap space that remains (although it does not
+	provide information on how the remaining heap might be fragmented). */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	task.  It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()).  If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
